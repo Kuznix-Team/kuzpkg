b50708cdd5f8bb045726f6929526e4326beccc98
Kuznix <krzysztofdemirkuzniak@gmail.com>
2026-08-20 09:25:29 +0200

    Update upgrade.c

--- src/kuzpkg/upgrade.c
+++ src/kuzpkg/upgrade.c
@@ -2,7 +2,6 @@
  *  upgrade.c
  *
  *  Copyright (C) 2026 Kuznix
- *  Copyright (c) 2002-2006 by Judd Vinet <jvinet@zeroflux.org>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
